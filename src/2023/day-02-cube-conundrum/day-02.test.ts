7a1bbff6a3b22e400803f27a6c24b45bd1b5a4d4
Koen Verburg <creativekoen@gmail.com>
2023-12-05 11:22:11 +0100

    feat(2023) finished day 2

--- src/2023/day-02-cube-conundrum/day-02.test.ts
+++ src/2023/day-02-cube-conundrum/day-02.test.ts
@@ -3,22 +3,20 @@ import { part1, part2 } from './day-02'
 
 test('part1 control', () => {
   const r = part1(prepareInput('./src/2023/day-02-cube-conundrum/data-control.txt'))
-  expect(r).toBe(24000)
+  expect(r).toBe(8)
 })
 
 test('part1 real ', () => {
   const r = part1(prepareInput('./src/2023/day-02-cube-conundrum/data-real.txt'))
-  expect(r).toBe(24000)
+  expect(r).toBe(2369)
 })
 
-/* test('part2 control', () => { */
-/*   const r = part2(prepareInput('./src/2023/day-02-cube-conundrum/data-control.txt')) */
-/*   expect(r).toBe(24000) */
-/* }) */
-/**/
-/* test('part2 real ', () => { */
-/*   const r = part2(prepareInput('./src/2023/day-02-cube-conundrum/data-real.txt')) */
-/*   expect(r).toBe(24000) */
-/* }) */
-/**/
-/**/
+test('part2 control', () => { 
+  const r = part2(prepareInput('./src/2023/day-02-cube-conundrum/data-control.txt'))
+  expect(r).toBe(2286) 
+})
+
+test('part2 real ', () => {
+  const r = part2(prepareInput('./src/2023/day-02-cube-conundrum/data-real.txt')) 
+  expect(r).toBe(66363) 
+}) 
--- src/2023/day-02-cube-conundrum/day-02.ts
+++ src/2023/day-02-cube-conundrum/day-02.ts
@@ -1,13 +1,63 @@
 import { count } from '../../helpers'
 
-export const part1 = (series: string[]) => {
-  const r = series.length
-
-  return count([])
+const TARGET_RED = 12
+const TARGET_BLUE = 14
+const TARGET_GREEN = 13
+const defaults = {
+  red: 0,
+  blue: 0,
+  green: 0,
 }
 
-export const part2 = (series: string[]) => {
-  const r = series.length
+const converToGame = (line: string) => {
+  const parts = line.split(':')
 
-  return count(r)
+  const id = parseInt(parts[0].split(' ')[1])
+  const bags = parts[1].split(';')
+
+  const reveals = bags.map((bag) => ({
+    ...defaults,
+    ...Object.fromEntries(
+      bag
+        .split(',')
+        .map((x) => x.trim())
+        .map((x) => x.split(' '))
+        .map(([count, color]) => [color, parseInt(count)]),
+    ),
+  }))
+
+  return {
+    id,
+    reveals,
+  }
+}
+
+export const part1 = (inputs: string[]) => {
+  const games = inputs
+    .filter(Boolean)
+    .map(converToGame)
+    .filter((x) =>
+      x.reveals.every(({ red, blue, green }) => red <= TARGET_RED && blue <= TARGET_BLUE && green <= TARGET_GREEN),
+    )
+    .map(({ id }) => id)
+
+  return count(games)
+}
+
+export const part2 = (inputs: string[]) => {
+  const games = inputs
+    .filter(Boolean)
+    .map(converToGame)
+    .map(({ reveals }) =>
+      reveals.reduce((acc, curr) => {
+        return {
+          red: Math.max(acc.red, curr.red),
+          blue: Math.max(acc.blue, curr.blue),
+          green: Math.max(acc.green, curr.green),
+        }
+      }, defaults),
+    )
+    .map(({ red, green, blue }) => red * green * blue)
+
+  return count(games)
 }
